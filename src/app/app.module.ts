e4b78f17ec74d4f31443b5f4ff97403a3034ab51
Mike McGuire <MoonshotMike@gmail.com>
2016-12-17 14:18:32 -0700

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NavbarComponent } from './components/navbar/navbar.component'
+}
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+      NavbarComponent
   ],
   imports: [
     BrowserModule,
