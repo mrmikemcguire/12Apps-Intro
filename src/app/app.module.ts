57d2dc85b575f973b11ef2e61c23cc3ecd7c509e
Mike McGuire <MoonshotMike@gmail.com>
2016-12-19 11:07:05 -0700

    clean copy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { NavbarComponent } from './components/navbar/navbar.component'
-}
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-      NavbarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
