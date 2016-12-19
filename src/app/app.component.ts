f9c640c01bbc3d4e0c17dbc77843caa0eeb38b7b
mcguire <mcguire@mullenhigh.com>
2016-12-19 11:43:57 -0700

    Going from PC to MacBook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Testing after HackHands';
+  title = 'Testing in the other direction after HackHands';
 }
