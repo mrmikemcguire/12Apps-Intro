00befcc1d5048979fd187b1aa9ecb2489c38a764
Mike McGuire <MoonshotMike@gmail.com>
2016-12-20 18:55:47 -0700

    basic jumbotron hookup achieved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent} from './components/navbar/navbar.component';
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+      JumbotronComponent
   ],
   imports: [
     BrowserModule
