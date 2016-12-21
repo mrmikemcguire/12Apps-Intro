58e1362a0b3e4fe39de7432820a3920012a2c918
Mike McGuire <MoonshotMike@gmail.com>
2016-12-20 19:04:57 -0700

    jumbotron content added

--- src/app/components/jumbotron/jumbotron.component.ts
+++ src/app/components/jumbotron/jumbotron.component.ts
@@ -5,4 +5,17 @@ import { Component } from '@angular/core';
     templateUrl: './jumbotron.component.html'
 })
 
-export class JumbotronComponent { }
+export class JumbotronComponent {
+    private jbtHeading:string;
+    private jbtText:string;
+    private jbtBtnText:string;
+    private jbtBtnUrl:string;
+
+    constructor(){
+        this.jbtHeading="Hello World";
+        this.jbtText="This is a simple hero unit.";
+        this.jbtBtnText="Read More";
+        this.jbtBtnUrl="/about"
+    }
+
+}
